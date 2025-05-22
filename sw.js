bbf851060c68a929f0540e2dff11923a14b9cdf3
ubunturikard <ubuntu@svenningsen.dk>
2025-05-22 17:06:20 +0200

    Tilføjer filer til GitHub Pages

--- sw.js
+++ sw.js
@@ -1,9 +1,48 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then((registration) => {
-      console.log('Service Worker registreret:', registration);
+const CACHE_NAME = 'shopping-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/indkoeb.html',
+  '/about.html',
+  '/manifest.json',
+  '/styles.css',
+  '/app.js',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+// Installer service worker og cache nødvendige filer
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching app-filer...');
+      return cache.addAll(urlsToCache);
     })
-    .catch((error) => {
-      console.log('Service Worker fejlede:', error);
-    });
-}
+  );
+});
+
+// Intercepter netværksforespørgsler og server cache-filer
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Opdater cache ved nye versioner
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            console.log('Sletter gammel cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
